b110b4a4a8e8884f535539771795543c8aef9640
Keenan McKenzie <keenan@mckenziemail.com>
2019-12-06 06:59:35 -0600

    add coverage to requirements and start adding test coverage

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,30 +1,18 @@
 import unittest
 from espn import app
 
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-class TestStringMethodsAgain(unittest.TestCase):
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
 class TestAppPages(unittest.TestCase):
    def setUp(self):
       self.app = app.test_client()
 
-   def test_main_page(self):
+   def test_leagueHistory_page(self):
        response = self.app.get("/leagueHistory/test", follow_redirects=True)
        self.assertEqual(response.status_code, 200)
+   
+   def test_owners_page(self):
+       response = self.app.get("/leagueHistory/owners/1107328", follow_redirects=True)
+       self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
