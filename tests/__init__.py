34b8038bad87a331aef33ee61d2deb894736a362
Keenan McKenzie <keenan@mckenziemail.com>
2019-12-07 14:26:05 -0600

    Add tests for all routes in leagueHistory blueprint

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,7 +12,22 @@ class TestAppPages(unittest.TestCase):
    def test_owners_page(self):
        response = self.app.get("/leagueHistory/owners/1107328", follow_redirects=True)
        self.assertEqual(response.status_code, 200)
+   
+   def test_records(self):
+       response = self.app.get("/leagueHistory/records/1107328", follow_redirects=True)
+       self.assertEqual(response.status_code, 200)
 
+   def test_years(self):
+       response = self.app.get("/leagueHistory/years/1107328", follow_redirects=True)
+       self.assertEqual(response.status_code, 200)
+
+   def test_alltime(self):
+       response = self.app.get("/leagueHistory/alltime/1107328", follow_redirects=True)
+       self.assertEqual(response.status_code, 200)
+
+   def test_year(self):
+       response = self.app.get("/leagueHistory/season/1107328/2018", follow_redirects=True)
+       self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
