c724f73e88e47f1fef20045b38b4c78b7e94e620
Keenan McKenzie <keenan@mckenziemail.com>
2019-12-15 12:13:08 -0600

    Fix playoff performance model so it pulls all years instead of 1

--- espn/playoffHistory/models.py
+++ espn/playoffHistory/models.py
@@ -48,22 +48,24 @@ def get_count_regular_season_games(league_id, year):
    wins = record_json['wins']
    losses = record_json['losses']
    ties = record_json['ties']
-   return (wins + losses + ties)
+   return wins + losses + ties
 
 def get_playoff_performance(league_id):
-   years = get_years_with_league_id(league_id)   
+   years = get_years_with_league_id(league_id)
+   print(years)   
    team_ids = get_teams_with_league_id(league_id, years)
    owners = get_owners_with_league_id(league_id, years)
-   regular_season_games = get_count_regular_season_games(league_id, years[0])
    playoff_record = {}
    total_postseason_games = 0
    for year in years:
+      regular_season_games = get_count_regular_season_games(league_id, year)
+      print("Regular Season Games: " + str(regular_season_games))
       url = "https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/" + str(league_id) + "?seasonId=" + str(year)
       r = requests.get(url, params={"view": "mMatchup"})
       json = r.json()[0]
       games = json['schedule']
       for game in games:
-         if game['matchupPeriodId'] >= regular_season_games:
+         if game['matchupPeriodId'] > regular_season_games:
             total_postseason_games += 1
             if game['winner'] == 'HOME':
                 winner_team_id = game['home']['teamId']
@@ -84,13 +86,13 @@ def get_playoff_performance(league_id):
             if loser_owner_id not in playoff_record:
                 playoff_record[loser_owner_id] = {
                     "name": owners[loser_owner_id],
-                    "wins": 1,
-                    "losses": 0
+                    "wins": 0,
+                    "losses": 1
                 }
             else:
-               playoff_record[loser_owner_id]["losses"] += 1
-      print(total_postseason_games)
-      return playoff_record
+                playoff_record[loser_owner_id]["losses"] += 1
+   print(total_postseason_games)
+   return playoff_record
 
 
 
