54a8206e5441f140b2412f6ca5501c2806490953
Keenan McKenzie <keenan@mckenziemail.com>
2020-01-06 12:55:10 -0600

    cleanup bad tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,22 +7,6 @@ class TestRegularSeasonPages(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_leagueHistory_page(self):
-        response = self.app.get("/regularSeason/test", follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_owners_page(self):
-        response = self.app.get("/regularSeason/owners/1107328", follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_records(self):
-        response = self.app.get("/regularSeason/records/1107328", follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-    def test_years(self):
-        response = self.app.get("/regularSeason/years/1107328", follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
     def test_year(self):
         response = self.app.get("/regularSeason/season/1107328/2018", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
@@ -39,10 +23,6 @@ class TestRegularSeasonPages(unittest.TestCase):
 class TestPostSeasonPages(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
-
-    def test_page(self):
-        response = self.app.get("/postseason/test", follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
 
     def test_performance(self):
         response = self.app.get('postseason/performance/1107328', follow_redirects=True)
