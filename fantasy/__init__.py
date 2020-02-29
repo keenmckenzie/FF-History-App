de29823c09fdf1d4c1e52000259536bd0ce9eb33
Keenan McKenzie <keenan@mckenziemail.com>
2020-02-29 07:27:22 -0800

    comment out yahoo commands

--- fantasy/__init__.py
+++ fantasy/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 
-from fantasy import yahoo, espn
+#from fantasy import yahoo, espn
+from fantasy import espn
 
 app = Flask(__name__)
 cors=CORS(app, resources={r"/*": {"origins": ["https://localhost*"]}})
 
-from fantasy.yahoo.routes import mod
+#from fantasy.yahoo.routes import mod
 from fantasy.espn.routes import mod
 
-app.register_blueprint(yahoo.routes.mod, url_prefix='/yahoo')
+#app.register_blueprint(yahoo.routes.mod, url_prefix='/yahoo')
 app.register_blueprint(espn.routes.mod, url_prefix='/espn')
